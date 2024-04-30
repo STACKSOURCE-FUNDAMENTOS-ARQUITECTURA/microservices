b0b264a465fba7bb58938ef198a64b75bd9ccfcc
MICHAELCARRILLO <u20201a924@upc.edu.pe>
2024-04-30 12:59:56 -0500

    feat: init

---
+++ microservices/employment-service/src/main/java/com/techlinker/employment_service/EmploymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.techlinker.employment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmploymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmploymentServiceApplication.class, args);
+	}
+
+}
---
+++ microservices/employment-service/src/test/java/com/techlinker/employment_service/EmploymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techlinker.employment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmploymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

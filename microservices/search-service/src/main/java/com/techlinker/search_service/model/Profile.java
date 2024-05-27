27fced351135b9c622e77196c9046f476fc19486
JohanMV <U20201C105@UPC.EDU.PE>
2024-05-26 19:32:58 -0500

    feat: Implement search functionality with basic query endpoint
    - Added Profile entity, repository, service, and controller
    - Configured PostgreSQL database connection
    - Set up Swagger for API documentation
    - Implemented basic GET /search endpoint to search profiles by query

---
+++ microservices/search-service/src/main/java/com/techlinker/search_service/model/Profile.java
@@ -0,0 +1,48 @@
+package com.techlinker.search_service.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+
+@Entity
+public class Profile {
+    @Id
+    private Long id;
+    private String name;
+    private String skill;
+    private String experience;
+
+    // Getters y setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSkill() {
+        return skill;
+    }
+
+    public void setSkill(String skill) {
+        this.skill = skill;
+    }
+
+    public String getExperience() {
+        return experience;
+    }
+
+    public void setExperience(String experience) {
+        this.experience = experience;
+    }
+}
